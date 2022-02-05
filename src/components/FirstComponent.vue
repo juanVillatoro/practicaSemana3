f5cf9999518a1b8e55956c705202284a623e8e28
Juan Villatoro <smis010420@ugb.edu.sv>
2022-02-05 00:19:42 -0600

    Agregando los archivos de la practica

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="firstcomponent">
+        <img v-on:mousemove="getXY" alt="Vue logo" src="../assets/logo.png">
+        <h1 v-on:mousemove="getXY" @mouseover="mouseOver" @mouseout="mouseOut"
+        >   
+            {{ titulo }}
+        </h1>
+        <h3 :style="[styleE,{'fontSize':fontSize+'px'}]">{{ materia }}</h3>
+        <p>Tamaño de la fuente {{ fontSize }}</p>
+        <button v-on:click="fontSize++">Incrementar</button>
+        <button v-on:click="fontSize--">Disminuir</button>
+
+        <h4>{{x}}, {{y}}</h4>
+        <b>{{message}}</b>
+
+        <input type="text" v-on:keydown>
+
+    </div>
+</template>
+<script>
+    export default {
+        name: 'FirstComponent',
+        data() {
+            return{
+                titulo: 'Mi primer componente',
+                materia:'Programacion Computacional IV',
+                x:0,
+                y:0,
+                message:'',
+                fontSize:10,
+
+            }
+        },
+        methods:{
+            getXY(event){
+                this.x=event.offsetX;
+                this.y=event.offsetY;
+            },
+            mouseOver(){
+                this.message = "IN"
+            },
+            mouseOut(){
+                this.message = "OUT"
+            },
+            key(event){
+                console.log
+            }
+        },
+        computed:{
+            styleE(){
+                return{
+                    textDecoration: 'underline',
+                    textWeigth: 'bold'
+                }
+            }
+        }
+    }
+</script>
